bca50541fa3deba52f835c3989caa032427dd3d0
Luiz Hélio <luizhelio.fim@outlook.com>
2025-06-11 20:22:11 -0300

    Update ClienteController.java

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class ClienteController {
+import java.util.List;
+import javax.swing.JOptionPane;
 
+import model.Cliente;
+import service.ClienteService;
+
+public class ClienteController {
+	
+	private final ClienteService clienteService;
+
+	public ClienteController() {
+		this.clienteService = new ClienteService();
+	}
+	
+	public void cadastrarCliente(Cliente cliente) {
+		try {
+			clienteService.cadastrarCliente(cliente);
+			JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso!");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro ao cadastrar cliente: " + e.getMessage());
+		}
+	}
+	
+	public Cliente buscarPorCpf(String cpf) {
+		return clienteService.buscarPorCpf(cpf);
+	}
+	
+	public List<Cliente> listarTodos() {
+		return clienteService.listarTodos();
+	}
+	
 }
