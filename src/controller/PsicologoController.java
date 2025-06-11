fcfed22d85781c16346cfe81b70e97491b028f3f
Luiz Hélio <luizhelio.fim@outlook.com>
2025-06-11 20:22:09 -0300

    Update PsicologoController.java

--- src/controller/PsicologoController.java
+++ src/controller/PsicologoController.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class PsicologoController {
+import java.util.List;
 
+import javax.swing.JOptionPane;
+
+import model.Psicologo;
+import service.PsicologoService;
+
+public class PsicologoController {
+	
+	private final PsicologoService psicologoService;
+	
+	public PsicologoController() {
+		this.psicologoService = new PsicologoService();
+	}
+	
+	public void cadastrarPsicologo(Psicologo psicologo) {
+		try {
+			psicologoService.cadastrarPsicologo(psicologo);
+			JOptionPane.showMessageDialog(null, "Psicologo cadastrado com sucesso!");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro ao cadastrar psicologo: " + e.getMessage());
+		}
+	}
+	
+	public Psicologo buscarPorCrp(String crp) {
+		return psicologoService.buscarPorCrp(crp);
+	}
+	
+	public List<Psicologo> listarTodos() {
+		return psicologoService.listarTodos();
+	}
 }
