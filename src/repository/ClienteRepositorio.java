4a158411dbafc5bded036757e83ca7d06443cc85
Luiz Hélio <luizhelio.fim@outlook.com>
2025-06-12 18:10:55 -0300

    adequação das classes do repositório

--- src/repository/ClienteRepositorio.java
+++ src/repository/ClienteRepositorio.java
@@ -1,6 +1,7 @@
 package repository;
 
 import model.Cliente;
+import model.perfil.PerfilCliente;
 
 public class ClienteRepositorio extends RepositorioCSV<Cliente> {
 
@@ -15,19 +16,30 @@ public class ClienteRepositorio extends RepositorioCSV<Cliente> {
     		   c.getNome() + "," + 
     		   c.getEmail() + "," + 
     		   c.getSenha() + "," +
-    		   c.getTelefone();
+    		   c.getTelefone() + "," +
+    		   c.getPerfil().getSexo() + "," +
+    		   c.getPerfil().getIdade() + "," +
+    		   c.getPerfil().getObservacoes();
     }
 
     @Override
     public Cliente converterDeCSV(String linha) {
-        String[] partes = linha.split(",");
-        Cliente c = new Cliente();
-        c.setId(Integer.parseInt(partes[0]));
-        c.setCpf(partes[1]);
-        c.setNome(partes[2]);
-        c.setEmail(partes[3]);
-        c.setSenha(partes[4]);
-        c.setTelefone(partes[5]);
-        return c;
+        String[] partes = linha.split(",", -1);
+
+        PerfilCliente perfil = new PerfilCliente(
+        		partes[6], 
+        		Integer.parseInt(partes[7]), 
+        		partes[8]);
+        
+
+        return new Cliente(
+            Integer.parseInt(partes[0]),
+            partes[1],
+            partes[2],
+            partes[3],
+            partes[4],
+            partes[5],
+            perfil
+        );
     }
 }
--- src/repository/PsicologoRepositorio.java
+++ src/repository/PsicologoRepositorio.java
@@ -1,6 +1,7 @@
 package repository;
 
 import model.Psicologo;
+import model.perfil.PerfilPsicologo;
 
 public class PsicologoRepositorio extends RepositorioCSV<Psicologo>{
 	
@@ -16,21 +17,32 @@ public class PsicologoRepositorio extends RepositorioCSV<Psicologo>{
     		   p.getEmail() + "," +
     		   p.getSenha() + "," +
     		   p.getCrp() + "," +
-    		   p.getEspecialidade();
+    		   p.getEspecialidade() + "," +
+    		   p.getPerfil().getAnosExperiencia() + "," +
+    		   p.getPerfil().getDescricao() + "," +
+    		   p.getPerfil().getHorarioAtendimento();
     }
 
     @Override
     public Psicologo converterDeCSV(String linha) {
-        String[] partes = linha.split(",");
-        Psicologo p = new Psicologo();
-        p.setId(Integer.parseInt(partes[0]));
-        p.setCpf(partes[1]);
-        p.setNome(partes[2]);
-        p.setEmail(partes[3]);
-        p.setSenha(partes[4]);
-        p.setCrp(partes[5]);
-        p.setEspecialidade(partes[6]);
-        return p;
+        String[] partes = linha.split(",", -1);
+
+        PerfilPsicologo perfil = new PerfilPsicologo(
+            Integer.parseInt(partes[7]),
+            partes[8],
+            partes[9]
+        );
+        
+        return new Psicologo(
+            Integer.parseInt(partes[0]),
+            partes[1],
+            partes[2],
+            partes[3],
+            partes[4],
+            partes[5],
+            partes[6],
+            perfil
+        );
     }
 
 }
