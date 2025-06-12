d16f76c0832ea422b279c3d5f502af4c0666249b
Luiz Hélio <luizhelio.fim@outlook.com>
2025-06-12 17:14:02 -0300

    Update Agenda.java

--- src/model/Agenda.java
+++ src/model/Agenda.java
@@ -1,15 +1,18 @@
+package model;
+
 
 public class Agenda {
 
-
 }
+
 public void adicionarConsulta(consultaId) {
 	
 }
+
 public void removerConsulta(consultaId) {
 	
 }
+
 public void reagendarConsulta(consultaId,novaData) {
 	
 }
-
