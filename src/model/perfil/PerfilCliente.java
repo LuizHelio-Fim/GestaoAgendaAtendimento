f9648c0425d939684c890c588d72f93f28f82a79
Luiz Hélio <luizhelio.fim@outlook.com>
2025-06-12 18:01:43 -0300

    Create Classes de perfil

--- src/model/perfil/PerfilCliente.java
+++ src/model/perfil/PerfilCliente.java
@@ -2,4 +2,40 @@ package model.perfil;
 
 public class PerfilCliente {
 
+    private String sexo;
+    private int idade;
+    private String observacoes; // informações adicionais do psicólogo
+
+    // Construtores
+    
+    public PerfilCliente() {}
+
+    public PerfilCliente(String sexo, int idade, String observacoes) {
+        this.sexo = sexo;
+        this.idade = idade;
+        this.observacoes = observacoes;
+    }
+
+    // Getters e Setters
+
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 }
--- src/model/perfil/PerfilPsicologo.java
+++ src/model/perfil/PerfilPsicologo.java
@@ -2,4 +2,40 @@ package model.perfil;
 
 public class PerfilPsicologo {
 
+    private int anosExperiencia;
+    private String descricao;
+    private String horarioAtendimento; 	// Seg a Sex, 9h-17h
+
+    // Construtores
+    
+    public PerfilPsicologo() {}
+
+    public PerfilPsicologo(int anosExperiencia, String descricao, String horarioAtendimento) {
+        this.anosExperiencia = anosExperiencia;
+        this.descricao = descricao;
+        this.horarioAtendimento = horarioAtendimento;
+    }
+
+    // Getters e Setters
+
+    public int getAnosExperiencia() {
+        return anosExperiencia;
+    }
+    public void setAnosExperiencia(int anosExperiencia) {
+        this.anosExperiencia = anosExperiencia;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getHorarioAtendimento() {
+        return horarioAtendimento;
+    }
+    public void setHorarioAtendimento(String horarioAtendimento) {
+        this.horarioAtendimento = horarioAtendimento;
+    }
 }
